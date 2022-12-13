46917ee3628a6b2789fe7bf349c036fac184ce79
Pedro Ruben <pedropulidoangel@gmail.com>
2022-12-13 09:03:47 +0100

    Initial commit

---
+++ src/test/java/com/w2m/tecnical/TecnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.tecnical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
